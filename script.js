7d862431e1bde2f1ba1b820543c0b866829963bb
Shivam Singla <shivamsingla491@gmail.com>
2022-05-22 14:16:57 +0530

    Arrow Functionality Achieved

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let imgData = {
-  img1: {
+  0: {
     previewImage:
       "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
     title: "cat.jpeg",
   },
-  img2: {
+  1: {
     previewImage:
       "https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
     title:
       "a man and a woman trying to cook a meal together in a modern kitchen.jpg",
   },
-  img3: {
+  2: {
     previewImage:
       "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
     title: "bali-kelingking-beach-plastic-removal-drive.key",
   },
-  img4: {
+  3: {
     previewImage:
       "https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
     title: "NextByk Investor Pitch 2022.ppt",
   },
-  img5: {
+  4: {
     previewImage:
       "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
     title: "interns-performance-report-may-2022.key",
@@ -44,12 +44,11 @@ function compute_new_title(title) {
 
 let leftChild = document.querySelector(".child-left");
 
-document.querySelector('input[name="img-title"]').value =
-  imgData["img1"]["title"];
+document.querySelector('input[name="img-title"]').value = imgData["0"]["title"];
 var flag_selected = 0;
 
 for (const key in imgData) {
-  //   console.log(`${key}: ${imgData[key]["title"]}`);
+  console.log(`${key}: ${imgData[key]["title"]}`);
   let new_img = document.createElement("div");
   if (flag_selected === 0) {
     flag_selected = 1;
@@ -70,11 +69,7 @@ for (const key in imgData) {
   leftChild.appendChild(new_img);
   new_img.addEventListener("click", (event) => {
     console.log("Image clicked : " + key);
-    let img_view = document.querySelector(".child-right .img-view img");
-    img_view.src = imgData[key]["previewImage"];
-    document.querySelector('input[name="img-title"]').value =
-      imgData[key]["title"];
-    select(event);
+    select(event, key, 0);
   });
 }
 
@@ -94,17 +89,47 @@ img_Title.addEventListener("keypress", function (e) {
     });
   }
 });
-
-function select(e) {
+var currentSelectedIndex = 0;
+function select(e, key, is_keypressed) {
+  let img_view = document.querySelector(".child-right .img-view img");
+  img_view.src = imgData[key]["previewImage"];
+  document.querySelector('input[name="img-title"]').value =
+    imgData[key]["title"];
+  currentSelectedIndex = key;
+  console.log("Index of image selected : ", currentSelectedIndex);
   // Unset selected class from other options
   const selected = document.querySelectorAll(".child-left .option-selected");
   selected.forEach(function (el) {
     el.classList.remove("option-selected");
   });
-  console.log(e.target.classList);
-  var x = e.target;
-  while (x.parentElement.classList[0] != "img-short") {
-    x = x.parentElement;
+
+  // console.log(e.target.classList);
+  if (is_keypressed == "0") {
+    var x = e.target;
+    while (x.parentElement.classList[0] != "img-short") {
+      x = x.parentElement;
+    }
+    x.parentElement.classList.add("option-selected");
+  } else {
+    document
+      .querySelector(".child-left")
+      .children[currentSelectedIndex].classList.add("option-selected");
   }
-  x.parentElement.classList.add("option-selected");
 }
+
+document.addEventListener("keydown", (event) => {
+  console.log("keypressed");
+  if (event.key == "ArrowDown") {
+    console.log("key down");
+    if (currentSelectedIndex < imgTitle.length - 1) {
+      select(event, parseInt(currentSelectedIndex) + 1, 1);
+    }
+  }
+  if (event.key == "ArrowUp") {
+    // select(event, key);
+    console.log("keyup");
+    if (currentSelectedIndex > 0) {
+      select(event, parseInt(currentSelectedIndex) - 1, 1);
+    }
+  }
+});
