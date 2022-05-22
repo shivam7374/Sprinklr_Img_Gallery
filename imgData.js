274999e52f43a4bfcb8f9fd6e5047be7cd66ceb3
Shivam Singla <shivamsingla491@gmail.com>
2022-05-22 22:36:04 +0530

    Code optimised and commented

---
+++ imgData.js
@@ -0,0 +1,37 @@
+let imgData = {
+  0: {
+    previewImage:
+      "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "cat.jpeg",
+  },
+  1: {
+    previewImage:
+      "https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title:
+      "a man and a woman trying to cook a meal together in a modern kitchen.jpg",
+  },
+  2: {
+    previewImage:
+      "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "bali-kelingking-beach-plastic-removal-drive.key",
+  },
+  3: {
+    previewImage:
+      "https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    title: "NextByk Investor Pitch 2022.ppt",
+  },
+  4: {
+    previewImage:
+      "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+    title: "interns-performance-report-may-2022.key",
+  },
+};
+let imgTitle = [
+  "cat.jpeg",
+  "a man and a woman trying to cook a meal together in a modern kitchen.jpg",
+  "bali-kelingking-beach-plastic-removal-drive.key",
+  "NextByk Investor Pitch 2022.ppt",
+  "interns-performance-report-may-2022.key",
+];
+
+export { imgData, imgTitle };
--- script.js
+++ script.js
@@ -1,92 +1,110 @@
-let imgData = {
-  0: {
-    previewImage:
-      "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-    title: "cat.jpeg",
-  },
-  1: {
-    previewImage:
-      "https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-    title:
-      "a man and a woman trying to cook a meal together in a modern kitchen.jpg",
-  },
-  2: {
-    previewImage:
-      "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-    title: "bali-kelingking-beach-plastic-removal-drive.key",
-  },
-  3: {
-    previewImage:
-      "https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-    title: "NextByk Investor Pitch 2022.ppt",
-  },
-  4: {
-    previewImage:
-      "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-    title: "interns-performance-report-may-2022.key",
-  },
-};
-let imgTitle = [
-  "cat.jpeg",
-  "a man and a woman trying to cook a meal together in a modern kitchen.jpg",
-  "bali-kelingking-beach-plastic-removal-drive.key",
-  "NextByk Investor Pitch 2022.ppt",
-  "interns-performance-report-may-2022.key",
-];
+import { imgData, imgTitle } from "./imgData.js";
 
 var currentSelectedIndex = 0;
+let leftChild = document.querySelector(".child-left");
+var flag_selected = 0;
 
+// function used to calculate the title and convert to abc...xyz format incase of large length
 function compute_new_title(title) {
+  // if length is less than 30 characters then show as it is
   if (title.length < 30) {
     return title;
   } else {
+    // in case length is greater than 30 characters than take first 12 characters and last 12 characters
     let new_title = title.slice(0, 12) + "..." + title.slice(title.length - 12);
     return new_title;
   }
 }
 
-let leftChild = document.querySelector(".child-left");
-
-document.querySelector('input[name="img-title"]').value = imgData["0"]["title"];
-var flag_selected = 0;
-
+// add img data imported to the HTML page by iterating over each element of imgData
 for (const key in imgData) {
+  setDataToHTML(key);
+}
+
+// set data to HTML in a organised manner
+function setDataToHTML(key) {
   console.log(`${key}: ${imgData[key]["title"]}`);
+  // create new conaier for the img to be added
   let new_img = document.createElement("div");
   if (flag_selected === 0) {
     flag_selected = 1;
+    // if intial no image selected so view the title in the input view of title
     new_img.className = "img-short option-selected";
+    document.querySelector('input[name="img-title"]').value =
+      imgData[key]["title"];
   } else {
     new_img.className = "img-short";
   }
+  // create the div element containing image tag
   let new_img_left = document.createElement("div");
   new_img_left.className = "img-short-left";
+  // create the div element containg title of image
   let new_img_right = document.createElement("div");
   new_img_right.className = "img-short-right";
+  // calculate the title according to syntax abc..xyz
   new_img_right.innerText = compute_new_title(imgData[key]["title"]);
+  // image element of the left side
   let img = document.createElement("img");
   img.src = imgData[key]["previewImage"];
+
+  // append the elements to provide perfect orientation
   new_img_left.appendChild(img);
   new_img.appendChild(new_img_left);
   new_img.appendChild(new_img_right);
   leftChild.appendChild(new_img);
+
+  // add event listener of click so on click we show coreesponding image and title
   new_img.addEventListener("click", (event) => {
     console.log("Image clicked : " + key);
     select(event, key, 0);
   });
 }
 
-let img_Title = document.querySelector('input[name="img-title"]');
-var currentTitle;
-img_Title.addEventListener("click", () => {
-  currentTitle = img_Title.value;
-});
+function select(e, key, is_keypressed) {
+  // update the image to the right side
+  let img_view = document.querySelector(".child-right .img-view img");
+  img_view.src = imgData[key]["previewImage"];
+  // update the title to the left
+  document.querySelector('input[name="img-title"]').value = imgTitle[key];
+  //update the current index of image
+  currentSelectedIndex = key;
+  console.log("Index of image selected : ", currentSelectedIndex);
+  // change color of selected image and unset rest of the images
+  changeSelectedBackground(e, is_keypressed);
+}
 
+function changeSelectedBackground(e, is_keypressed) {
+  const selected = document.querySelectorAll(".child-left .option-selected");
+  // unset all the images
+  selected.forEach(function (el) {
+    el.classList.remove("option-selected");
+  });
+
+  // console.log(e.target.classList);
+  if (is_keypressed == "0") {
+    // if the item was clicked then we have to find the element where we want to add the class option selected
+    var x = e.target;
+    while (x.parentElement.classList[0] != "img-short") {
+      x = x.parentElement;
+    }
+    x.parentElement.classList.add("option-selected");
+  } else {
+    // when the key was pressed we already gave the element where we had to add the class
+    document
+      .querySelector(".child-left")
+      .children[currentSelectedIndex].classList.add("option-selected");
+  }
+}
+
+let img_Title = document.querySelector('input[name="img-title"]');
+
+// add event listener for title change and upadate to left side along with the database of imgTitle
 img_Title.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     // code for enter
     console.log("maybe the title changed");
     var newTitle = document.querySelector('input[name="img-title"]').value;
+    // current index tracks the image to be shown
     imgTitle[parseInt(currentSelectedIndex)] = newTitle;
     document.querySelector(".child-left").children[
       currentSelectedIndex
@@ -94,40 +112,18 @@ img_Title.addEventListener("keypress", function (e) {
   }
 });
 
-function select(e, key, is_keypressed) {
-  let img_view = document.querySelector(".child-right .img-view img");
-  img_view.src = imgData[key]["previewImage"];
-  document.querySelector('input[name="img-title"]').value = imgTitle[key];
-  currentSelectedIndex = key;
-  console.log("Index of image selected : ", currentSelectedIndex);
-  // Unset selected class from other options
-  const selected = document.querySelectorAll(".child-left .option-selected");
-  selected.forEach(function (el) {
-    el.classList.remove("option-selected");
-  });
-
-  // console.log(e.target.classList);
-  if (is_keypressed == "0") {
-    var x = e.target;
-    while (x.parentElement.classList[0] != "img-short") {
-      x = x.parentElement;
-    }
-    x.parentElement.classList.add("option-selected");
-  } else {
-    document
-      .querySelector(".child-left")
-      .children[currentSelectedIndex].classList.add("option-selected");
-  }
-}
-
+// add event listener when some keyboard key is pressed
 document.addEventListener("keydown", (event) => {
   console.log("keypressed");
+  // if the key is arrow key down
   if (event.key == "ArrowDown") {
     console.log("key down");
     if (currentSelectedIndex < imgTitle.length - 1) {
       select(event, parseInt(currentSelectedIndex) + 1, 1);
     }
   }
+
+  // if the key is arrow key up
   if (event.key == "ArrowUp") {
     // select(event, key);
     console.log("keyup");
